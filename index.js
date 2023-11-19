f92446321ca32db11871b235b4cb37d21e03d139
WaiYanHtun <waiyanhtunsince2000@gmail.com>
2023-11-19 07:37:30 +0630

    The whole game implemented! Still need to work on UI

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ function getComputerChoice(){
         return "Scissors"
     }
 }
+
+let computer = 0;
+let playerSelection = 'rock';
+let computerSelection = 'rock'; 
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock'){
+        ++computer;
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper'){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        return "Draw!";
+    }
+}
+
+
+function game(){
+  
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("Type in your selection!").toLowerCase();
+        computerSelection = 'rock';
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if(computer >= 3){
+        console.log("Computer Wins");
+    }else{
+        console.log("Player Wins");
+    }
+}
+
+
+
+
