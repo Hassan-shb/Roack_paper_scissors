8d07a24b4c3f5b71452a5ceafe9099948a0a3569
WaiYanHtun <waiyanhtunsince2000@gmail.com>
2023-11-22 13:42:56 +0630

    The click events of rps implemented

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        playerSelection = e.target.innerText;
+        console.log(playerSelection);
+        game();
+    });
+    
+})
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     if(choice === 0){
@@ -9,13 +20,13 @@ function getComputerChoice(){
     }
 }
 
-let computer = 0;
+
 let playerSelection = 'rock';
 let computerSelection = 'rock'; 
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock'){
-        ++computer;
+    console.log(playerSelection, computerSelection);
+    if(playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock'){ 
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }else if(playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper'){
         return `You win! ${playerSelection} beats ${computerSelection}`;
@@ -26,16 +37,9 @@ function playRound(playerSelection, computerSelection){
 
 
 function game(){
-    for(let i = 0; i < 5; i++){
-        playerSelection = prompt("Type in your selection!").toLowerCase();
-        computerSelection = 'rock';
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = getComputerChoice().toLowerCase();
         console.log(playRound(playerSelection, computerSelection));
-    }
-    if(computer >= 3){
-        console.log("Computer Wins");
-    }else{
-        console.log("Player Wins");
-    }
 }
 
 
